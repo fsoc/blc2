3ec0ec5029dce88f1ace7b4a7738be8201edac44
fsoc <fsoc@altai.se>
2015-10-04 15:55:26 +0200

    Remade the code with handlebars.

--- blc2.js
+++ blc2.js
@@ -1,17 +1,9 @@
+Bookings = new Mongo.Collection("bookings");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    bookings: function() {
+      return Bookings.find({});
     }
   });
 }
