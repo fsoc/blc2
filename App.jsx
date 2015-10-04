e595b20878df4ba345010af37e7590965c6973a0
fsoc <fsoc@altai.se>
2015-10-04 10:36:52 +0200

    Reactified the first example.

--- App.jsx
+++ App.jsx
@@ -1,7 +1,18 @@
 App = React.createClass({
+  getInitialState: function() {
+    return { counter: 0 };
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.setState({ counter: this.state.counter + 1 });
+  },
   render() {
     return (
-      <p>hello</p>
+      <div>
+      <button className="btn" onClick={this.handleSubmit} >Click Me</button>
+      <p>You've pressed the button {this.state.counter} times.</p>
+      </div>
+
       );
   }
 });
